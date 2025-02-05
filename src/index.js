751fdc822a326671773d24db3f6f998b9806964c
3n3a <46775561+3n3a@users.noreply.github.com>
2025-02-05 20:42:09 +0100

    add cors headers

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ export default {
 	async fetch(request, env, ctx) {
 		// Cloudflare sets the client IP in the "cf-connecting-ip" header.
 		const ip = request.headers.get('cf-connecting-ip') || 'IP not found';
-  
+
 		return new Response(ip, {
-		  headers: { 'Content-Type': 'text/plain' }
+		  headers: {
+			  'Content-Type': 'text/plain',
+			  'Access-Control-Allow-Methods': 'GET',
+                          'Access-Control-Allow-Origin': '*'
+		  }
 		});
 	},
 };

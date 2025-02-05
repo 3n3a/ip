313da89ecc3332e8f75ab75239fa0eaec3d2f910
3n3a <46775561+3n3a@users.noreply.github.com>
2025-02-05 20:09:29 +0100

    update to use cf wrangler hello world template

---
+++ src/index.js
@@ -0,0 +1,10 @@
+export default {
+	async fetch(request, env, ctx) {
+		// Cloudflare sets the client IP in the "cf-connecting-ip" header.
+		const ip = request.headers.get('cf-connecting-ip') || 'IP not found';
+  
+		return new Response(ip, {
+		  headers: { 'Content-Type': 'text/plain' }
+		});
+	},
+};
